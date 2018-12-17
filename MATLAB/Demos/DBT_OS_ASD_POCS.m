155e69c0e018b8d03fdb1efff5ee72793e5d1591
Pranjal Sahu <pranjalsahu5@gmail.com>
2018-12-16 19:12:39 -0500

    adding ordered subsets ASD POCS code

--- MATLAB/Demos/DBT_OS_ASD_POCS.m
+++ MATLAB/Demos/DBT_OS_ASD_POCS.m
@@ -14,14 +14,14 @@ close all;
 % 
 %
 % Settings for CE-12 (Working with 9.1 and compute 30)
-% filepath = '/media/pranjal/de24af8d-2361-4ea2-a07a-1801b54488d9/DBT_recon_data/CE12/';
-% sx_a   = 1800;
-% sy_a   = 3584;
-% slices = 46;
-% sx_b   = 2600;
-% sy_b   = 1300;
-% volume_name   = 'CE-12_2600x1300_46.raw';
-% offdetector_height = 35;
+filepath = '/media/pranjal/de24af8d-2361-4ea2-a07a-1801b54488d9/DBT_recon_data/CE12/';
+sx_a   = 1800;
+sy_a   = 3584;
+slices = 46;
+sx_b   = 2600;
+sy_b   = 1300;
+volume_name   = 'CE-12_2600x1300_46_os_asd_pocs1.raw';
+offdetector_height = 35;
 %
 % % Settings for CE-14 (Working with 9.1 and compute 30)
 % filepath = '/media/pranjal/de24af8d-2361-4ea2-a07a-1801b54488d9/DBT_recon_data/CE14/';
@@ -84,14 +84,14 @@ close all;
 % offdetector_height = 55;
 %
 %
-filepath = '/media/pranjal/2d33dff3-95f7-4dc0-9842-a9b18bcf1bf9/pranjal/DBT_data/projections/70_250/';
-sx_a   = 1800;
-sy_a   = 3584;
-slices = 50;
-sx_b   = 2400;
-sy_b   = 1100;
-volume_name   = 'CE_2400x1100_50.raw';
-offdetector_height = 15;
+% filepath = '/media/pranjal/2d33dff3-95f7-4dc0-9842-a9b18bcf1bf9/pranjal/DBT_data/projections/70_250/';
+% sx_a   = 1800;
+% sy_a   = 3584;
+% slices = 50;
+% sx_b   = 2400;
+% sy_b   = 1100;
+% volume_name   = 'CE_2400x1100_50_2.raw';
+% offdetector_height = 15;
 
 
 
@@ -101,7 +101,7 @@ offdetector_height = 15;
 %-------------------------------------------------------------------------------------
 
 anglefile       = strcat(filepath, 'angles.ini');
-projections_dir = strcat(filepath, 'Projections_Renamed_Seg');
+projections_dir = strcat(filepath, 'Projections/Projections_Renamed_Seg');
 volume_path     = strcat(filepath,  volume_name); 
 
 
@@ -184,7 +184,7 @@ alpha=0.2;
 %   'TViter':      Defines the amount of TV iterations performed per SART
 %                  iteration. Default is 20
 
-ng=25;
+ng=50;
 
 % Other optional parameters
 % ----------------------------------------------
