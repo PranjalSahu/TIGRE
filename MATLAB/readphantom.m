bdd7c9a4d849b55b1cc6ea32b6ff6ba7704e5cea
Pranjal Sahu <pranjalsahu5@gmail.com>
2018-12-20 18:21:32 -0500

    Adding permutation of dimension in phantom to get correct dimension of volume

--- MATLAB/readphantom.m
+++ MATLAB/readphantom.m
@@ -2,8 +2,12 @@ function phantom=readphantom(path, phantomshape)
     fid  = fopen(path, 'r');
     data = fread(fid, phantomshape(1)*phantomshape(2)*phantomshape(3), 'char');  %change the shape of phantom 
     phantom = reshape(data, [phantomshape(1), phantomshape(2), phantomshape(3)]);
+    phantom = permute(phantom, [3  1 2]);
 end
 
+
+% Voxel size in the phantom is
+% 0.2 x 0.2 x 0.2
 
 % show one slice of phantom
 % imshow(reshape(phantom(:, 120, :), [329, 939])/10.0);
@@ -13,3 +17,9 @@ end
 % Do it for each slice and then just do the binary multiplcation with the
 % volume while back-projecting
 %imshow(imerode(imdilate(tp>th,se7),se9));
+
+
+%fid   = fopen('/home/pranjal/victre/breastPhantom/pc_2011766737_crop.raw', 'r');
+%data  = fread(fid, 388*932*419);
+%data1 = reshape(data, [388, 932, 419]);
+%imshow(data1(:, :, 450)/100.0);
