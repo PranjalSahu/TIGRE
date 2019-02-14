4cda0646682bfcd4e6d06484de40a72686990c4a
Pranjal Sahu <pranjalsahu5@gmail.com>
2019-02-14 12:34:32 -0500

    storing the values of the statistics

--- MATLAB/read_duke.m
+++ MATLAB/read_duke.m
@@ -8,10 +8,11 @@ while 1
     end
     names = [names; data];
 end
-disp(names);
+%disp(names);
 
 statspath = '/media/pranjal/de24af8d-2361-4ea2-a07a-1801b54488d9/duke_phantom/Cohort1/Cohort1.1uncompressed/Cohort1.1uncompressed/';
 
+all_arr = [];
 for i = 1 : length(names)
     name = names(i, :);
     p = strcat(statspath, name, '/', name, '_statistics.txt');
@@ -20,6 +21,24 @@ for i = 1 : length(names)
     fid  = fopen(p, 'r');
     data = fgetl(fid);
     data = fgetl(fid);
-    disp(name);
+    
     disp(data);
+    s = strsplit(data);
+    
+    arr = [];%zeros(1, 6);
+    if length(s) == 8
+        for tw=4:8
+            arr(tw-3) = str2num(char(s(tw)));
+        end
+        arr(6) = arr(5);
+    else
+        for tw=4:9
+            arr(tw-3) = str2num(char(s(tw)));
+        end
+    end
+    
+    all_arr = [all_arr; arr];
 end
+
+
+all_values = [];
